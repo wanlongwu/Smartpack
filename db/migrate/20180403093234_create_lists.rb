4518f8cc2ee11d216ef6d3465236662f2f10351c
Wanlong Wu <wumalone@gmail.com>
2018-04-03 19:08:25 +0900

    finished models

--- db/migrate/20180403093234_create_lists.rb
+++ db/migrate/20180403093234_create_lists.rb
@@ -1,7 +1,7 @@
 class CreateLists < ActiveRecord::Migration[5.1]
   def change
     create_table :lists do |t|
-      t.reference :user
+      t.references :user, foreign_key: true
       t.text :item
 
       t.timestamps
